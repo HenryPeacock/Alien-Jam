31a35de542f99463cd660837cf2235ea50ce9989
Lewis Remington <s5063237@bournemouth.ac.uk>
2019-11-04 22:39:48 +0000

    added half of the menus

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject OptionMenu;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ToOptionsMenu()
+    {
+        gameObject.SetActive(false);
+        OptionMenu.SetActive(true);
+    }
+}
---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public GameObject MainMenu;
+    public AudioMixer audioMixer;
+
+    public void BackButton()
+    {
+        gameObject.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+
+    //Allows the slider to control the volume
+    public void SetVolume(float _volume)
+    {
+        audioMixer.SetFloat("Volume", _volume);
+    }
+
+}
